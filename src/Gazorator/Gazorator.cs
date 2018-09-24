00a521641e16687d31ce544614b1e435c94362aa
Martin Björkström <martin.bjorkstrom@gmail.com>
2018-09-25 00:11:53 +0300

    Add support for references

--- src/Gazorator/Gazorator.cs
+++ src/Gazorator/Gazorator.cs
@@ -1,4 +1,7 @@
-﻿using System.IO;
+﻿using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Reflection;
 using System.Threading.Tasks;
 using Gazorator.Razor;
 using Gazorator.Scripting;
@@ -8,35 +11,42 @@ namespace Gazorator
     public abstract class Gazorator
     {
         protected TextWriter Output { get; }
+        protected IEnumerable<Assembly> References { get; } = new List<Assembly>();
 
-        protected Gazorator(TextWriter output = null)
+        protected Gazorator(TextWriter output = null, params Assembly[] references)
         {
             Output = output ?? TextWriter.Null;
+            References = new List<Assembly>(references);
         }
 
         public static Gazorator Default => new DefaultGazorator(TextWriter.Null);
 
         public Gazorator<TModel> WithModel<TModel>(TModel model)
         {
-            return new Gazorator<TModel>(Output, model);
+            return new Gazorator<TModel>(Output, model, References.ToArray());
         }
 
         public Gazorator WithOutput(TextWriter output)
         {
-            return new DefaultGazorator(output);
+            return new DefaultGazorator(output, References.ToArray());
+        }
+
+        public Gazorator WithReferences(params Assembly[] references)
+        {
+            return new DefaultGazorator(Output, references);
         }
 
         public virtual Task ProcessAsync(string filePath)
         {
             var razorGenerator = new CSharpScriptRazorGenerator(Path.GetDirectoryName(filePath));
             var csharpScript = razorGenerator.Generate(filePath);
-            var razorContentGenerator = new RazorContentGenerator(Output);
+            var razorContentGenerator = new RazorContentGenerator(Output, References);
             return razorContentGenerator.Generate(csharpScript);
         }
 
         private sealed class DefaultGazorator : Gazorator
         {
-            public DefaultGazorator(TextWriter output = null) : base(output)
+            public DefaultGazorator(TextWriter output = null, params Assembly[] references) : base(output, references)
             {
             }
         }
@@ -46,21 +56,26 @@ namespace Gazorator
     {
         private readonly TModel _model;
 
-        internal Gazorator(TextWriter output, TModel model) : base(output)
+        internal Gazorator(TextWriter output, TModel model, params Assembly[] references) : base(output, references)
         {
             _model = model;
         }
 
         public new Gazorator<TModel> WithOutput(TextWriter output)
         {
-            return new Gazorator<TModel>(output, _model);
+            return new Gazorator<TModel>(output, _model, References.ToArray());
+        }
+
+        public new Gazorator<TModel> WithReferences(params Assembly[] references)
+        {
+            return new Gazorator<TModel>(Output, _model, references);
         }
 
         public override Task ProcessAsync(string filePath)
         {
             var razorGenerator = new CSharpScriptRazorGenerator(Path.GetDirectoryName(filePath));
             var csharpScript = razorGenerator.Generate(filePath);
-            var razorContentGenerator = new RazorContentGenerator<TModel>(_model, Output);
+            var razorContentGenerator = new RazorContentGenerator<TModel>(_model, Output, References);
             return razorContentGenerator.Generate(csharpScript);
         }
     }
--- src/Gazorator/Scripting/RazorContentGenerator.cs
+++ src/Gazorator/Scripting/RazorContentGenerator.cs
@@ -14,6 +14,13 @@ namespace Gazorator.Scripting
 {
     internal abstract class RazorContentGeneratorBase
     {
+        private IReadOnlyCollection<Assembly> _references;
+
+        protected RazorContentGeneratorBase(IEnumerable<Assembly> references)
+        {
+            _references = new List<Assembly>(references);
+        }
+
         public Task Generate(string csharpScript)
         {
             var options = ScriptOptions.Default
@@ -37,12 +44,24 @@ namespace Gazorator.Scripting
 
         protected virtual IEnumerable<MetadataReference> GetMetadataReferences()
         {
+            yield return MetadataReference.CreateFromFile(typeof(Action).Assembly.Location); // mscorlib or System.Private.Core
+            yield return MetadataReference.CreateFromFile(typeof(IQueryable).Assembly.Location); // System.Core or System.Linq.Expressions
+            yield return MetadataReference.CreateFromFile(typeof(Uri).Assembly.Location); // System
+            yield return MetadataReference.CreateFromFile(typeof(System.Xml.XmlReader).Assembly.Location); // System.Xml
+            yield return MetadataReference.CreateFromFile(typeof(System.Xml.Linq.XDocument).Assembly.Location); // System.Xml.Linq
+            yield return MetadataReference.CreateFromFile(typeof(System.Data.DataTable).Assembly.Location); // System.Data
+
             var entryAssembly = Assembly.GetEntryAssembly();
             yield return MetadataReference.CreateFromFile(entryAssembly.Location);
 
             foreach (var reference in entryAssembly.GetReferencedAssemblies())
             {
                 yield return MetadataReference.CreateFromFile(Assembly.Load(reference).Location);
+            }
+
+            foreach (var reference in _references)
+            {
+                yield return MetadataReference.CreateFromFile(reference.Location);
             }
         }
 
@@ -55,7 +74,7 @@ namespace Gazorator.Scripting
     {
         private readonly TextWriter _textWriter;
 
-        public RazorContentGenerator(TextWriter textWriter)
+        public RazorContentGenerator(TextWriter textWriter, IEnumerable<Assembly> references) : base(references)
         {
             _textWriter = textWriter ?? throw new ArgumentNullException(nameof(textWriter));
         }
@@ -77,7 +96,7 @@ namespace Gazorator.Scripting
         private readonly TModel _model;
         private readonly bool _isDynamicAssembly;
 
-        public RazorContentGenerator(TModel model, TextWriter textWriter)
+        public RazorContentGenerator(TModel model, TextWriter textWriter, IEnumerable<Assembly> references) : base(references)
         {
             _textWriter = textWriter ?? throw new ArgumentNullException(nameof(textWriter));
             if (typeof(TModel).IsNullable() && model == null)
